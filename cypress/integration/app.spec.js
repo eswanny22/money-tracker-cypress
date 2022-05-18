c44fc7cf0d4b4ab7b66470db56a107616560f30f
Eric Swanson <ejswanson22@gmail.com>
2022-05-18 15:03:41 -0500

    updated cypress visit timeout

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -5,10 +5,10 @@ describe("Money Tracker", () => {
         indexedDB.deleteDatabase("_pouch_accounts")
         indexedDB.deleteDatabase("_pouch_transactions")
         indexedDB.deleteDatabase("_pouch_tags")
+        cy.visit("http://localhost:3000", {timeout: 30000})
     });
 
     it("adds an account", () => {
-        cy.visit("http://localhost:3000")
         cy.get('input[placeholder*="Account name"]').type("test")
         cy.get('input[placeholder*="Balance"]').type("10000")
         cy.contains('Save Account').click()
