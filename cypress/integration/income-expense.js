7411f919425582585a3881d9ecfa1e63638b522d
Eric Swanson <ejswanson22@gmail.com>
2022-05-23 19:25:21 -0500

    failed test push

--- cypress/integration/income-expense.js
+++ cypress/integration/income-expense.js
@@ -5,7 +5,7 @@ describe("Money Tracker UI Income/Expense", () => {
         indexedDB.deleteDatabase("_pouch_accounts")
         indexedDB.deleteDatabase("_pouch_transactions")
         indexedDB.deleteDatabase("_pouch_tags")
-        cy.visit("http://localhost:3000")
+        cy.visit("http://localhost:300")
         cy.get('input[placeholder*="Account name"]').type("test")
         cy.get('input[placeholder*="Balance"]').type("10000")
         cy.contains('Save Account').click()
